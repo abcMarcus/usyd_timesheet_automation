b2552c7f1c4c7279d1c38cd4793b38809aefcf96
Sayuri Nekomiya <BBH@awsl.rip>
2025-03-06 05:10:34 +1100

    Fully automated login process

--- next_timesheet.py
+++ next_timesheet.py
@@ -1,6 +1,30 @@
 import sys
 import csv
+import io
 from datetime import datetime, timedelta
+
+def update_csv(path) -> str:
+    with open(path, mode='r+', newline='', encoding='utf-8') as f:
+        out = io.StringIO()
+        reader = csv.reader(f)
+        writer = csv.writer(out)
+
+        for row in reader:
+            if row:
+                original_date = datetime.strptime(row[0], "%d/%m/%Y")
+                new_date = original_date + timedelta(days=7)
+                row[0] = new_date.strftime("%d/%m/%Y")
+                writer.writerow(row)
+            else:
+                writer.writerow([])
+
+        f.seek(0)
+        f.truncate(0)
+
+        content = out.getvalue()
+        f.write(content)
+
+    return content
 
 def add_days_to_csv(infile, outfile):
     with open(infile, mode='r', newline='', encoding='utf-8') as fin, \
@@ -12,7 +36,7 @@ def add_days_to_csv(infile, outfile):
         for row in reader:
             if row:
                 original_date = datetime.strptime(row[0], "%d/%m/%Y")
-                new_date = original_date + timedelta(days=14)
+                new_date = original_date + timedelta(days=7)
                 row[0] = new_date.strftime("%d/%m/%Y")
                 writer.writerow(row)
             else:
